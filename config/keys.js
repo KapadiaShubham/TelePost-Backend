91327163704403c03a3b693471184aac1e1e49db
Shubham Kapadia <shubhamkapadia0@gmail.com>
2021-08-08 17:35:46 +0530

    Backend

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // mongoURL: 'mongodb+srv://shubham:shubham@cluster0.obf15.mongodb.net/database?retryWrites=true&w=majority',
+  mongoURL: process.env.mongoURL,
+  secretOrKey: process.env.secretOrKey || 'secret',
+}
